65ee131d85f90ac27976dda8bcd3aa5771d28529
abishkar <abishkar@abishkars-MacBook-Pro.local>
2020-05-02 07:56:44 +0545

    added initial code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="small-container">
+    <h1>Patients</h1>
+
+    <patient-table :patients="patients" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PatientTable from './components/PatientTable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PatientTable
+  },
+  data() {
+    return {
+      patients: [
+        {
+          id: 1,
+          name: 'Richard Hendricks',
+          email: 'richard@piedpiper.com',
+        },
+        {
+          id: 2,
+          name: 'Bertram Gilfoyle',
+          email: 'gilfoyle@piedpiper.com',
+        },
+        {
+          id: 3,
+          name: 'Dinesh Chugtai',
+          email: 'dinesh@piedpiper.com',
+        },
+      ],
+    }
   }
 }
 </script>
---
+++ src/components/PatientTable.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="patient-table">
+    <table>
+      <!-- ...thead... -->
+      <tbody>
+        <tr v-for="patient in patients" :key="patient.id">
+          <td>{{ patient.name }}</td>
+          <td>{{ patient.email }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'patient-table',
+    props: {
+    patients: Array,
+  },
+  }
+</script>
+
+<style scoped></style>
